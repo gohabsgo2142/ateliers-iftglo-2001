7503d2fd42607ee30895f4303622e6f13dee18d5
William Guimont-Martin <wil9876543210@hotmail.com>
2023-07-09 20:27:14 -0400

    wip

--- helper/prepare_bashrc.py
+++ helper/prepare_bashrc.py
@@ -1,9 +1,9 @@
 
-with open('~/.bashrc', 'a') as f:
+with open('/home/glo2001/.bashrc', 'a') as f:
     f.write('export PATH=$PATH:~/.atelier/ateliers-iftglo-2001/correction')
     f.write('export PATH=$PATH:~/.local/bin')
 
-with open('~/.gitconfig', 'a') as f:
+with open('/home/glo2001/.gitconfig', 'a') as f:
     f.write('''[user]
     email = glo2001@ulaval.ca
     name = Glo 2001
