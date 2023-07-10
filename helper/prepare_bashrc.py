4c7f6a97e0d2ef1148c153f0fd057f4c9d5605ae
William Guimont-Martin <wil9876543210@hotmail.com>
2023-07-09 20:25:18 -0400

    wip

--- helper/prepare_bashrc.py
+++ helper/prepare_bashrc.py
@@ -1,6 +1,6 @@
 
 with open('~/.bashrc', 'a') as f:
-    f.write('export PATH=$PATH:~/.ateliers/ateliers-iftglo-2001/atelier_bash/correction')
+    f.write('export PATH=$PATH:~/.atelier/ateliers-iftglo-2001/correction')
     f.write('export PATH=$PATH:~/.local/bin')
 
 with open('~/.gitconfig', 'a') as f:
