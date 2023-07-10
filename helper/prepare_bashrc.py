ed3a05b236dd140579e30c94cff48d2b28b1ac1a
William Guimont-Martin <wil9876543210@hotmail.com>
2023-07-09 20:28:08 -0400

    wip

--- helper/prepare_bashrc.py
+++ helper/prepare_bashrc.py
@@ -1,7 +1,7 @@
 
 with open('/home/glo2001/.bashrc', 'a') as f:
-    f.write('export PATH=$PATH:~/.atelier/ateliers-iftglo-2001/correction')
-    f.write('export PATH=$PATH:~/.local/bin')
+    f.write('export PATH=$PATH:~/.atelier/ateliers-iftglo-2001/correction\n')
+    f.write('export PATH=$PATH:~/.local/bin\n')
 
 with open('/home/glo2001/.gitconfig', 'a') as f:
     f.write('''[user]
